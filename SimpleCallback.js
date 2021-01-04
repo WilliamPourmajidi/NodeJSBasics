b9f7993d390cc9f831c8b7c15434b6d4010c2577
William Pourmajidi <William.Pourmajidi@gmail.com>
2021-01-04 14:08:57 -0500

    Simple Callback Example

--- SimpleCallback.js
+++ SimpleCallback.js
@@ -7,9 +7,14 @@ let multiply = function (a,b){
 
 let calc = function (num1,num2,callback){
     console.info("I am here in the calc function");
-    return callback(num1,num2);
-}
+    if (typeof callback === "function"){
+        return callback(num1,num2);
+    };
+
+
+};
 
 console.log(calc(12,2,add));
 console.log(calc(12,2,multiply));
+
 
