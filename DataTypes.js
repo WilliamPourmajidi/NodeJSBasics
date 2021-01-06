778a48fd26745e005a988ebfe1f749b60a81d2d7
William Pourmajidi <William.Pourmajidi@gmail.com>
2021-01-06 09:32:32 -0500

    Simple Data Types in JS

--- DataTypes.js
+++ DataTypes.js
@@ -4,7 +4,7 @@ var my_object = {
     firstName: "William", lastName: "Pourmajidi", fullname: function () {
         return this.firstName + " " + this.lastName;
     }
-};    // Object
+};    // Object (mind that the last one (fullname) is an object method (functions defined in an object)
 var my_boolean = true;
 console.log(my_number, my_string, my_object, my_boolean);
 console.log(typeof (my_number), typeof (my_string), typeof (my_object), typeof (my_boolean));
@@ -16,6 +16,7 @@ console.log(my_object.fullname())
 var person = {firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
 person = null;    // Now value is null, but type is still an object
 console.log(typeof (person))
+
 // Now let's use undefined
 var person = {firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
 person = undefined;   // Now both value and type is undefined
