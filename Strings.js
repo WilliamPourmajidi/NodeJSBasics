c7433b937bf04f7c102e07ae4738bcf7980368fd
William Pourmajidi <William.Pourmajidi@gmail.com>
2021-01-06 11:23:09 -0500

    Strings in JS

--- Strings.js
+++ Strings.js
@@ -1,4 +1,3 @@
-
 //Mixing quotation marks
 var string_1 = 'Albert Einstein said "Problems can not be solved by the same level of thinking that has created them" and I totally agree with it'
 console.log(string_1)
@@ -21,8 +20,8 @@ console.log(string_3)
 var string_4 = new String('This is a new string')    //an object
 var string_5 = 'This is a new string'
 
-console.log(string_4==string_5)   //true because both have the same value
-console.log(string_4===string_5)  // false because although the same value, but they are from different types
+console.log(string_4 == string_5)   //true because both have the same value
+console.log(string_4 === string_5)  // false because although the same value, but they are from different types
 
 //Finding a String in a String
 var string_6 = 'This was a test string and we want to search something in it'
@@ -36,6 +35,20 @@ var pos_2 = string_6.search('was not')
 
 console.log(pos_1)
 console.log(pos_2)  //returns -1 as it can not find 'was' in the given string
+
+// The slice() Method
+var string_7 = "MAC OS, Windows, Android, iOS ";
+var sliced_1 = string_7.slice(8, 15);
+var sliced_2 = string_7.slice(-4, -1);  //negative means start from the end of the string
+
+console.log(sliced_1)   // Windows
+console.log(sliced_2)   // iOS
+
+//The substr() Method
+//substr() is similar to slice() but the second parameter specifies the length of the extracted part
+var string_8 = "MAC OS, Windows, Android, iOS ";
+var substr = string_7.substr(17, 7);
+console.log(substr)   // Android
 
 
 
