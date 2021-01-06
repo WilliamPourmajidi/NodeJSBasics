23ebd26d0a27e956aa2168be2f673e6eebadbcb7
William Pourmajidi <William.Pourmajidi@gmail.com>
2021-01-06 17:00:37 -0500

    Functions in JS

--- Functions.js
+++ Functions.js
@@ -5,26 +5,26 @@ function isNil(value) {
     return value == null;
 };
 
-console.log('Testing the declared function',isNil());
-console.log('Testing the declared function',isNil(''));
+console.log('Testing the declared function',isNil());  //returns true
+console.log('Testing the declared function',isNil('')); //returns true
 
 
 // Function expression: starts with "const"
-const isTruthy = function (value) {
-    return !!value;
+const revert = function (value) {
+    return !value;
 };
 
-console.log('Testing the expression',isTruthy(true));
-console.log('Testing the expression',isTruthy(false));
+console.log('Testing the expression',revert(true)); //returns Testing the declared function false
+console.log('Testing the expression',revert(false)); //returns Testing the declared function true
 
 // Function expression: second example
-const getType = function funName(variable) {
+var getType = function funName(variable) {
     console.log('inside the function', typeof funName); // => 'function'
     return typeof variable;
 }
 console.log(getType(3));     // => 'number'
 console.log(getType.name);   // => 'funName'
-console.log(typeof getType); // => 'function'
+console.log(typeof (getType)); // => 'function'
 console.log('Outside the function', typeof funName); // => 'undefined'
 // note that  the variable funName is accessible within function scope, but not outside.
 
