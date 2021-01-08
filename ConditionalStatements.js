d361b68a9679a306d55ab611fcfc561544fcd889
William Pourmajidi <William.Pourmajidi@gmail.com>
2021-01-08 12:50:06 -0500

    Conditional Statements

--- ConditionalStatements.js
+++ ConditionalStatements.js
@@ -6,3 +6,25 @@ if (a == b) {
 } else {
     console.log(`numbers are not the same!`);
 }
+
+c = 6
+d = "6"
+
+if (c == d) {
+    console.log(`to our surprise, this is true!`)
+}
+
+if (c === d) {   //returns : how about now?!
+    console.log(`to our surprise, this is true!`)
+}
+else {
+    console.log(`how about now?!`)
+}
+
+e = 5
+f = 5
+g = 6
+//ternary operator (3-pieces)
+e == f ? console.log(`match`): console.log(`no match`);  // returns: match
+e == g ? console.log(`match`): console.log(`no match`); // returns: no match
+
